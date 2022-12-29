05a6be61e69c289bfe04cb5b810c345c009f6e7c
kimseungwoo <aoome123@naver.com>
2022-12-30 03:37:06 +0900

    problem4

--- problem4.py
+++ problem4.py
@@ -1,9 +1,8 @@
 # 문자열 뒤집는 함수 작성
 
 def reversestr(self, s:list[str]) -> None :
-    left, right = 0, len(s) -1
-    while left < right :
-        s[left], s[right] = s[right],s[left]
-        left+=1
-        right-=1
-    print(s)
+    left, right = 0, len(s) -1 # 0부터 세기에 각각 0, len(s) -1 값을 넣어준다.
+    while left < right : # 둘이 가로지를 때 그만
+        s[left], s[right] = s[right],s[left] # left에 right, right에 left를 통해 반대값을 줌
+        left+=1 # 1씩 증가
+        right-=1 # 1씩 감소
