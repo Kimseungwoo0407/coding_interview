1c82acb8a78efa9f8b4cc2559e27a600425cb8dc
kimseungwoo <aoome123@naver.com>
2022-12-30 03:34:02 +0900

    problem4

--- problem4.py
+++ problem4.py
@@ -1,7 +1,5 @@
 # 문자열 뒤집는 함수 작성
 
-str = ['h','e','l','l','o']
-
 def reversestr(self, s:list[str]) -> None :
     left, right = 0, len(s) -1
     while left < right :
@@ -9,5 +7,3 @@ def reversestr(self, s:list[str]) -> None :
         left+=1
         right-=1
     print(s)
-
-reversestr(str,str)
