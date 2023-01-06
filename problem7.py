4688e087dedc702b0f0e23b3dfad495368e2c30c
kimseungwoo <aoome123@naver.com>
2023-01-06 21:46:38 +0900

    problem7

--- problem7.py
+++ problem7.py
@@ -1,8 +1,8 @@
 # 가장흔한단어
 
 def mostCommonWord(self, paragraph: str, banned: list[str]) -> str :
-    words = [word for word in re.sub(r'[^\w]',' ', paragraph)
-        .lower().split()
+    words = [word for word in re.sub(r'[^\w]',' ', paragraph) # 단어 문자가 아닌 모든 문자를 공백으로 치환
+            .lower().split()
             if word not in banned]
     counts = collections.Counter(words)
 
